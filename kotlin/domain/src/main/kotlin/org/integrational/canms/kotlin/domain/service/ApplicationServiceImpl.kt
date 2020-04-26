3486b73584d2c15d2f91220bc478f8748e4a5813
GeraldLoeffler <gerald.loeffler@googlemail.com>
2020-04-26 12:59:10 +0200

    better build and publish

--- kotlin/domain/src/main/kotlin/org/integrational/canms/kotlin/domain/service/ApplicationServiceImpl.kt
+++ kotlin/domain/src/main/kotlin/org/integrational/canms/kotlin/domain/service/ApplicationServiceImpl.kt
@@ -2,8 +2,13 @@ package org.integrational.canms.kotlin.domain.service
 
 import org.integrational.canms.kotlin.domain.model.DomainEntity
 import org.integrational.canms.kotlin.domain.repo.DomainEntityRepo
+import javax.inject.Inject
+import javax.inject.Named
+import javax.inject.Singleton
 
-class ApplicationServiceImpl(private val repo: DomainEntityRepo) : ApplicationService {
+@Named
+@Singleton
+class ApplicationServiceImpl @Inject constructor(private val repo: DomainEntityRepo) : ApplicationService {
     override fun addOrUpdateDomainEntity(e: DomainEntity) = repo.createOrUpdate(e)
     override fun getDomainEntity(id: String) = repo.read(id)
     override fun getAllDomainEntities() = repo.readAll()
